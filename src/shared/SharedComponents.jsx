1fae40f6a8967e05a9e81a95375dd80ca88503df
chacka1315 <chacka131@gmail.com>
2025-10-05 17:06:12 +0000

    Finish writing the shared components. It provides custome input and button.

--- src/shared/SharedComponents.jsx
+++ src/shared/SharedComponents.jsx
@@ -1,12 +1,29 @@
-export function Button({ onClick, name = 'button', type = 'button' }) {
+export function Button({
+  onClick,
+  name = 'button',
+  type = 'button',
+  id = '',
+  ariaLabel,
+}) {
   return (
-    <button onClick={onClick} type={type}>
+    <button
+      onClick={onClick}
+      type={type}
+      id={id}
+      {...(ariaLabel ? { 'aria-label': ariaLabel } : {})}
+    >
       {name}
     </button>
   );
 }
 
-export function Input({ onChange, id, placeholder, type = 'text', value }) {
+export function Input({
+  onChange,
+  id = '',
+  placeholder = '',
+  type = 'text',
+  value,
+}) {
   return (
     <input
       type={type}
