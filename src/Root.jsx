4cb7422c8580caa550f764d3d23dd20ec0b742c7
chacka1315 <chacka131@gmail.com>
2025-10-05 17:14:08 +0000

    Spacing correction for redeability and fixing styles

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,12 +2,55 @@ import Header from './features/Header';
 import Footer from './features/Footer';
 import { Outlet } from 'react-router';
 import styles from './Root.module.css';
+import { useEffect, useMemo, useState } from 'react';
+import { getCartCount } from './utils';
+import getStoreItems from './features/APIHandler';
 
 function Root() {
+  const [cart, setCart] = useState([]);
+  const [shopData, setShopData] = useState(null);
+  const [items, setItems] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const cartCount = useMemo(() => getCartCount(cart), [cart]);
+
+  useEffect(() => {
+    const fetchShopItems = async () => {
+      try {
+        const data = await getStoreItems();
+        setItems(data);
+        setShopData(data);
+        setError(null);
+      } catch (error) {
+        setError(error.message);
+        setItems(null);
+        setShopData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchShopItems();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Header />
-      <Outlet />
+      <Header
+        cartCount={cartCount}
+        updateItems={setItems}
+        shopData={shopData}
+      />
+      <Outlet
+        context={{
+          cart,
+          updateCart: setCart,
+          items,
+          cartCount,
+          loading,
+          error,
+        }}
+      />
       <Footer />
     </div>
   );
